2753bb4fd17a6a30b553dc1f9771e5f61d197ec7
msko <kms89123@gmail.com>
2022-02-06 00:59:26 +0900

    Implement atom

---
+++ src/features/TodoList/atom.ts
@@ -0,0 +1,37 @@
+import { atom, atomFamily, selectorFamily } from 'recoil';
+import { isSameDay } from '../../utils/date';
+
+export interface Todo {
+  id: string;
+  content: string;
+  done: boolean;
+  date: Date;
+}
+
+export const todoListState = atom<Array<Todo>>({
+  key: 'todoListState',
+  default: [],
+});
+
+export const selectedDateState = atom<Date>({
+  key: 'selectedDateState',
+  default: new Date(),
+});
+
+export const selectedTodoSate = atom<Todo | null>({
+  key: 'selectedTodoSate',
+  default: null,
+});
+
+export const filteredTodoListState = atomFamily<Array<Todo>, Date>({
+  key: 'filteredTodoListState',
+  default: selectorFamily({
+    key: 'filteredTodoListState/default',
+    get:
+      (selectedDate) =>
+      ({ get }) => {
+        const todoList = get(todoListState);
+        return todoList.filter((todo) => isSameDay(todo.date, selectedDate));
+      },
+  }),
+});
