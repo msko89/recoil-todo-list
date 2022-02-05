97caf488a333a517ba7597f351b68b5af8af6e92
msko <kms89123@gmail.com>
2022-02-05 19:44:21 +0900

    Initial Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { atom, selector, useRecoilValue } from 'recoil';
+
+interface Props {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const todoIdState = atom({
+  key: 'todoIdState',
+  default: 1,
+});
+
+const todoItemQuery = selector({
+  key: 'todoItemQuery',
+  get: async ({ get }) => {
+    const id = get(todoIdState);
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+
+    return response.data;
+  },
+});
 
 function App() {
+  const { userId, title } = useRecoilValue(todoItemQuery);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{userId}</div>
+      <div>{title}</div>
+    </>
   );
 }
 
